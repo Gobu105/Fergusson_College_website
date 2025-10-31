2ff689d02d49654c71f4f8cf41307eac544bae67
Jatin Joshi <119153089+Gobu105@users.noreply.github.com>
2025-10-31 08:59:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fergusson College</title>
+  <link rel="icon" type="image/x-icon" href="/images/images.png">
   <link rel="stylesheet" href="script/styles.css">
   <script src="script/script.js" defer></script>
 </head>
