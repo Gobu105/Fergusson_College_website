7d4784973d25c463762eb19d3c731551023c2549
Jatin Joshi <119153089+Gobu105@users.noreply.github.com>
2025-11-05 12:22:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fergusson College</title>
   <link rel="icon" type="image/x-icon" href="/images/images.png">
-  <link rel="stylesheet" href="script/styles.css?v=1">
+  <link rel="stylesheet" href="script/styles.css">
 </head>
 <body>
 
