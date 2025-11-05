128c4333dfcfc5ca53e6f53c4f86e1da81e86285
Gobu105 <jatinjoshi105@gmail.com>
2025-11-05 12:46:06 +0530

    mobile

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fergusson College</title>
   <link rel="icon" type="image/x-icon" href="/images/images.png">
-  <link rel="stylesheet" href="script/styles.css">
+  <link rel="stylesheet" href="script/styles.css?v=6">
   <script src="script/script.js" defer></script>
 </head>
 <body>
